8a03b00e8717bae3c7b31d7004345bedfa58f26a
branduren <branduren@gmail.com>
2013-04-14 01:24:55 +0200

    magit added to key binding
    to use type: C-x g

--- cfg-binding.el
+++ cfg-binding.el
@@ -1,21 +1,24 @@
 ;;; cfg-binding.el begin
 
-;; Complete with hippie
+;; complete with hippie
 (global-set-key (kbd "C-i") 'hippie-expand)
 
 ;; use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;; Window switching. (C-x o, C-x O)
+;; window switching. (C-x o, C-x O)
 (global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
 
-;; Start a new eshell even if one is active.
+;; start a new eshell even if one is active.
 (global-set-key (kbd "C-x m") (lambda () (interactive) (eshell t)))
 
-;; Same as shell, and keep fingers on the home row
+;; same as shell, and keep fingers on the home row
 (global-set-key (kbd "C-h") 'backward-delete-char)
 
-;; Help
+;; so brilliant
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; help
 (global-set-key "\M-h" 'help)
 
 (provide 'cfg-binding)
